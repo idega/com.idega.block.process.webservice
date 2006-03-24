207a3885021c07ed75f1e3ed9d320a90f277e427
Thomas Hilbig <thomas@idega.com>
2006-03-24 15:19:04 +0000

    added basic authentication

--- src/java/TestClient.java
+++ src/java/TestClient.java
@@ -1,11 +1,12 @@
 import java.net.URL;
-
+import org.apache.axis.client.Stub;
 import com.idega.block.process.webservice.server.CaseEntry;
 import com.idega.block.process.webservice.server.CaseResult;
 import com.idega.block.process.webservice.server.CaseService;
 import com.idega.block.process.webservice.server.CaseServiceServiceLocator;
 import com.idega.block.process.webservice.server.Item;
 import com.idega.block.process.webservice.server.Owner;
+import com.idega.block.process.wsclient.WSCaseConstants;
 import com.idega.util.IWTimestamp;
 
 public class TestClient {
@@ -15,7 +16,7 @@ public class TestClient {
 	// private static String endpoint =
 	// "http://213.167.155.187:6580/GoProHusavik.nsf/webservicenewcase?OpenAgent";
 	private static String endpoint = "http://localhost:9090/services/CaseService";
-	//private static String endpoint = "http://rrtest.rvk.is/services/CaseService";
+	// private static String endpoint = "http://rrtest.rvk.is/services/CaseService";
 	// private static String endpoint =
 	// "http://localhost:8090/rvk/services/CaseService";
 	//private static String endpoint = "http://213.167.155.148/Case/Case_NewCase_SoapPort.asmx?op=NewCase";
@@ -35,31 +36,42 @@ public class TestClient {
 	private static void testVUF() {
 		try {
 			Owner owner = new Owner();
-			owner.setAddress("Havallagata 13");
+			owner.setAddress("Tjarnarmýri 5");
 			owner.setCity("Reykjavik");
-			owner.setEmail("tryggvi@idega.is");
-			owner.setGsm("6954451");
-			owner.setName("Tryggvi Larusson");
-			owner.setPostalcode("101");
-			owner.setSocialsecurity("1011783159");
+			owner.setEmail("hello@idega.is");
+			owner.setGsm("00000");
+			owner.setName("");
+			owner.setPostalcode("170");
+			//owner.setSocialsecurity("12");
+			owner.setSocialsecurity("1703532189");
 			CaseEntry wsCase = new CaseEntry();
 			//wsCase.setStatus(WSCaseConstants.STATUS_CLOSED);
 			wsCase.setCode("JOBA");
 			wsCase.setCreated(IWTimestamp.RightNow().toSQLDateString());
 			wsCase.setModified(IWTimestamp.RightNow().toSQLDateString());
-			wsCase.setExternalCase_id("AG-UHE-0000203");
-			wsCase.setSubject("starfsumsokn");
+			wsCase.setExternalCase_id("AAC-UHE-0000203");
+			wsCase.setSubject("Hello venusv starfsumsokn");
 			wsCase.setStatus("UBEH");
 			wsCase.setOwner(owner);
 			//wsCase.setMessage("my message");
-			Item[] items = new Item[1];
-			items[0] = new Item();
+			Item[] items = new Item[4];
+			for (int i = 0; i < items.length; i++) {
+				items[i] = new Item();
+			}
 			items[0].setKey("CASE_URL");
 			items[0].setValue("http://vuf.rvk.is/umsokn?id=AG-UHE-0000201&kt=2004522249");
+			items[1] = null;
+			items[2].setKey(WSCaseConstants.MAIL_MESSAGE_SUBJECT);
+			items[2].setValue("My subject");
+			items[3].setKey(WSCaseConstants.MAIL_MESSAGE_BODY);
+			items[3].setValue("Hi Thomas! Hvað segir þú?");
 			wsCase.setMetadata(items);
 			CaseServiceServiceLocator service = new CaseServiceServiceLocator();
 			// Now use the service to get a stub which implements the SDI.
 			CaseService port = service.getCaseService(new URL(endpoint)); //
+			Stub stub = (Stub) port;
+			stub.setUsername("Thoma");
+			stub.setPassword("Weser");
 			//NewCasePort port = service.getCreateNewCaseHttp();
 			CaseResult ret = port.createOrUpdateCase(wsCase);
 			System.out.println("Sent 'Hello!', got operation='" + ret.getOperation() + "'and id=" + ret.getId());
